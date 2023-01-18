c2885a7081123dc20bd8834050243545836015e9
yjh0816 <2543kt@naver.com>
2023-01-18 14:52:24 +0900

    feat: Implement api write

--- product/views.py
+++ product/views.py
@@ -1,8 +1,22 @@
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Product
 
 class ProductListView(APIView):
+    def post(self, request, *args, **kwargs):
+        product = Product(
+            name = request.data.get('name'),
+            price = request.data.get('price'),
+            product_type = request.data.get('product_type'),
+        )
+        product.save()
+        return Response({
+            "id": product.id,
+            "name": product.name,
+            "price": product.price,
+            "product_type": product.product_type,
+        },status=status.HTTP_201_CREATED)
 
     def get(self, request, *args, **kwargs):
         ret = []
@@ -21,6 +35,7 @@ class ProductListView(APIView):
         return Response(ret)
 
 class ProductDetailView(APIView):
+
     def get(self, request, *args, **kwargs):
         product = Product.objects.get(pk=request.path_info.split('/')[-1])
 
