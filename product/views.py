1b183d14691a90652b6a28c8c7855f9a0ee79b79
yjh0816 <2543kt@naver.com>
2023-01-19 09:47:51 +0900

    feat: Update read,write api using generics,mixins

--- product/views.py
+++ product/views.py
@@ -4,19 +4,28 @@ from .serializers import ProductSerializer
 
 class ProductListView(
     mixins.ListModelMixin, 
-    # mixins.CreateModelMixin, 
+    mixins.CreateModelMixin, 
     generics.GenericAPIView,
 ):
     serializer_class = ProductSerializer
 
     def get_queryset(self):
-        return Product.objects.all().order_by('id')
+        products = Product.objects.all()
+
+        if 'name' in self.request.query_params:
+            name = self.request.query_params.get('name')
+            products = products.filter(name__contains=name)
+        # if 'price' in self.request.query_params:
+        #     price = self.request.query_params.get('price')
+        #     products = products.filter(price__lte=price)
+
+        return products.order_by('id')
 
     def get(self, request, *args, **kwargs):
         return self.list(request, args, kwargs)
 
-    # def post(self, request, *args, **kwargs):
-    #     return self.create(request, args, kwargs)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, args, kwargs)
 
 #################################################
 #                This is legacy                 #
