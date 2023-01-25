40908e40e31539c4edec06d2da7fdf1a838e76ae
yjh0816 <2543kt@naver.com>
2023-01-25 15:05:32 +0900

    feat: Implement product detail page

--- product/serializers.py
+++ product/serializers.py
@@ -16,6 +16,18 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class CommentSerializer(serializers.ModelSerializer):
+    product_name = serializers.SerializerMethodField()
+    member_username = serializers.SerializerMethodField()
+    tstamp = serializers.DateTimeField(
+        read_only=True, format='%Y-%m-%d %H:%M:%S'
+    )
+
+    def get_product_name(self, obj):
+        return obj.product.name
+
+    def get_member_username(self, obj):
+        return obj.member.username
+
     class Meta:
         model = Comment
         fields = '__all__'
